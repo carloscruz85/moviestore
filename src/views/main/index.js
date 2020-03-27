afd68003832fdccecc4705d314bb54f03283b7e1
carloscruz85 <carloscruz85@gmail.com>
2020-03-27 13:31:06 -0600

    Creating basic main view

---
+++ src/views/main/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './index.scss';
+
+
+//ctrl merge
+
+class Main extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+
+  render(){
+      return(
+        <div className="main-container">
+         I am the Main
+        </div>
+      )
+    }
+}
+
+export default Main;
