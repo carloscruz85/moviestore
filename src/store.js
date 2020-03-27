06278a5346f0d345221d80b31da4428a3b7023cf
carloscruz85 <carloscruz85@gmail.com>
2020-03-27 13:15:57 -0600

    Redux store added

---
+++ src/store.js
@@ -0,0 +1,18 @@
+import {createStore} from 'redux';
+const reducer = (state, action) => {
+//EXAMPLE CODE
+	// switch(action.type){
+// 	case "LOGIN":
+// 		return{
+// 		...state,
+// 		user: action.data,
+// 		login: true
+// 	}
+// 	default :
+// 		return state
+// 	}
+}
+
+export default createStore(reducer, {
+	host: 'http://carloscruz85.com/movies/'
+})
