c8311a67a9534b5640874f94b4178855d3b97647
carloscruz85 <carloscruz85@gmail.com>
2020-03-28 14:30:58 -0600

    All proptypes configure

--- src/components/buttons/button/index.js
+++ src/components/buttons/button/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import './index.scss';
+import React from "react";
+import "./index.scss";
+import PropTypes from "prop-types";
 
-class Button extends React.Component{
-
-  render(){
-      return(
-      <button className="basic-button" onClick={() => {this.props.customClickEvent()}}>{this.props.text}</button>
-      )
-    }
+class Button extends React.Component {
+  render() {
+    return (
+      <button
+        className="basic-button"
+        onClick={() => {
+          this.props.customClickEvent();
+        }}
+      >
+        {this.props.text}
+      </button>
+    );
+  }
 }
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired
+};
 
 export default Button;
--- src/components/overlayer/index.js
+++ src/components/overlayer/index.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import './index.scss';
-class OverLayer extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
+import React from "react";
+import "./index.scss";
+import PropTypes from "prop-types";
 
-    }
+class OverLayer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
-  render(){
-      return(
-        <div className="overlayer">
-          {this.props.msg}
-        </div>
-      )
-    }
+  render() {
+    return <div className="overlayer">{this.props.msg}</div>;
+  }
 }
 
-
-export default OverLayer
+OverLayer.propTypes = {
+  msg: PropTypes.string.isRequired
+};
+export default OverLayer;
