249f599da437c8835d3a20689110e2d0e0544f21
carloscruz85 <carloscruz85@gmail.com>
2020-03-29 18:32:58 -0600

    Show movies only if are avaible for normal users, and show always to admins

--- src/views/moviestore/index.js
+++ src/views/moviestore/index.js
@@ -478,114 +478,117 @@ class VideoStore extends React.Component {
 
           {movies.map((movie, imovie) => {
             // console.log(movie);
+            if (this.props.isAdmin === true || movie.availability > 0)
+              return (
+                <div key={imovie}>
+                  <div
+                    className="movie-card"
+                    style={{ backgroundImage: `url(${movie.fimg_url})` }}
+                    onClick={() => this.switchDescription(imovie)}
+                  ></div>
+                  <div>
+                    {movie.show === "true" ? (
+                      <div className="movie-description">
+                        <div className="form-container">
+                          {this.props.isAdmin ? (
+                            <div>
+                              <Input
+                                label="Title"
+                                type="text"
+                                name={imovie + "|title"}
+                                value={movie.title.rendered}
+                                customChange={this.handleMovieInput.bind(this)}
+                              />
+                              <Input
+                                label="Description"
+                                type="text"
+                                name={imovie + "|description"}
+                                value={movie.description}
+                                customChange={this.handleMovieInput.bind(this)}
+                              />
+                              <Input
+                                label="Stock"
+                                type="number"
+                                name={imovie + "|stock"}
+                                value={movie.stock}
+                                customChange={this.handleMovieInput.bind(this)}
+                              />
+                              <Input
+                                label="Rental Price"
+                                type="number"
+                                name={imovie + "|rental_price"}
+                                value={movie.rental_price}
+                                customChange={this.handleMovieInput.bind(this)}
+                              />
+                              <Input
+                                label="Sale Price"
+                                type="number"
+                                name={imovie + "|sale_price"}
+                                value={movie.sale_price}
+                                customChange={this.handleMovieInput.bind(this)}
+                              />
+                              <Input
+                                label="Availability"
+                                type="number"
+                                name={imovie + "|availability"}
+                                value={movie.availability}
+                                customChange={this.handleMovieInput.bind(this)}
+                              />
 
-            return (
-              <div key={imovie}>
-                <div
-                  className="movie-card"
-                  style={{ backgroundImage: `url(${movie.fimg_url})` }}
-                  onClick={() => this.switchDescription(imovie)}
-                ></div>
-                <div>
-                  {movie.show === "true" ? (
-                    <div className="movie-description">
-                      <div className="form-container">
-                        {this.props.isAdmin ? (
-                          <div>
-                            <Input
-                              label="Title"
-                              type="text"
-                              name={imovie + "|title"}
-                              value={movie.title.rendered}
-                              customChange={this.handleMovieInput.bind(this)}
-                            />
-                            <Input
-                              label="Description"
-                              type="text"
-                              name={imovie + "|description"}
-                              value={movie.description}
-                              customChange={this.handleMovieInput.bind(this)}
-                            />
-                            <Input
-                              label="Stock"
-                              type="number"
-                              name={imovie + "|stock"}
-                              value={movie.stock}
-                              customChange={this.handleMovieInput.bind(this)}
-                            />
-                            <Input
-                              label="Rental Price"
-                              type="number"
-                              name={imovie + "|rental_price"}
-                              value={movie.rental_price}
-                              customChange={this.handleMovieInput.bind(this)}
-                            />
-                            <Input
-                              label="Sale Price"
-                              type="number"
-                              name={imovie + "|sale_price"}
-                              value={movie.sale_price}
-                              customChange={this.handleMovieInput.bind(this)}
-                            />
-                            <Input
-                              label="Availability"
-                              type="number"
-                              name={imovie + "|availability"}
-                              value={movie.availability}
-                              customChange={this.handleMovieInput.bind(this)}
-                            />
-
-                            <button
-                              className="green-button"
-                              onClick={() => this.saveMovie(imovie)}
-                            >
-                              Save Movie
-                            </button>
-                            <button
-                              className="yellow-button"
-                              onClick={() => this.deleteMovie(imovie)}
-                            >
-                              Delete Movie
-                            </button>
-                          </div>
-                        ) : (
-                          <div>
-                            <h3>{movie.title.rendered}</h3>
-                            <div className="color-yellow">
-                              {this.getLikes(imovie)} <div>Likes</div>
-                              {this.iLiked(imovie) ? (
-                                <IoIosHeartDislike
-                                  className="color-red"
-                                  onClick={() => this.like(imovie)}
-                                />
-                              ) : (
-                                <IoIosHeart
-                                  className="color-red"
-                                  onClick={() => this.like(imovie)}
-                                />
-                              )}
+                              <button
+                                className="green-button"
+                                onClick={() => this.saveMovie(imovie)}
+                              >
+                                Save Movie
+                              </button>
+                              <button
+                                className="yellow-button"
+                                onClick={() => this.deleteMovie(imovie)}
+                              >
+                                Delete Movie
+                              </button>
                             </div>
-                            <p className="card-list">
-                              Description: {movie.description}
-                            </p>
-                            <p className="card-list">
-                              Rental Price: ${movie.rental_price}
-                            </p>
-                            <p className="card-list">
-                              Sale Price: ${movie.sale_price}
-                            </p>
-                          </div>
-                        )}
+                          ) : (
+                            <div>
+                              <h3>{movie.title.rendered}</h3>
+                              <div className="color-yellow">
+                                {this.getLikes(imovie)} <div>Likes</div>
+                                {this.iLiked(imovie) ? (
+                                  <IoIosHeartDislike
+                                    className="color-red"
+                                    onClick={() => this.like(imovie)}
+                                  />
+                                ) : (
+                                  <IoIosHeart
+                                    className="color-red"
+                                    onClick={() => this.like(imovie)}
+                                  />
+                                )}
+                              </div>
+                              <p className="card-list">
+                                Description: {movie.description}
+                              </p>
+                              <p className="card-list">
+                                Rental Price: ${movie.rental_price}
+                              </p>
+                              <p className="card-list">
+                                Sale Price: ${movie.sale_price}
+                              </p>
+                            </div>
+                          )}
 
-                        <button onClick={() => this.switchDescription(imovie)}>
-                          Close
-                        </button>
+                          <button
+                            onClick={() => this.switchDescription(imovie)}
+                          >
+                            Close
+                          </button>
+                        </div>
                       </div>
-                    </div>
-                  ) : null}
+                    ) : null}
+                  </div>
                 </div>
-              </div>
-            );
+              );
+            else return null;
           })}
         </div>
       </div>
